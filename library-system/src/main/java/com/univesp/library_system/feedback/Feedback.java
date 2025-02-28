84d9b256e758870e18c212094826ec079bd3f4b7
Dyastrophism <guilhermesouzamaciel1@gmail.com>
2025-02-28 09:00:54 -0300

    feedback entity

--- library-system/src/main/java/com/univesp/library_system/feedback/Feedback.java
+++ library-system/src/main/java/com/univesp/library_system/feedback/Feedback.java
@@ -1,4 +1,4 @@
-package com.univesp.library_system.book;
+package com.univesp.library_system.feedback;
 
 import jakarta.persistence.*;
 import org.springframework.data.annotation.CreatedBy;
@@ -11,18 +11,13 @@ import java.time.LocalDateTime;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-public class Book {
+public class Feedback {
 
     @Id
     @GeneratedValue
     private Integer id;
-    private String title;
-    private String authorName;
-    private String isbn;
-    private String synopsis;
-    private String bookCover;
-    private boolean archived;
-    private boolean shareable;
+    private Double note;
+    private String comment;
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdDate;
